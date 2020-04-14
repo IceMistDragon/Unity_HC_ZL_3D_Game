8cefb3a10bd9577a7385438661cdf9b483488033
IceMistDragon <43175064+IceMistDragon@users.noreply.github.com>
2020-04-14 21:41:56 +0800

    介面操作

---
+++ 3Dgame/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform dragon;
+
+    [Header("追蹤速度"), Range(0, 100)]
+    public float speed;
+
+    private void Track()
+    {
+        Vector3 draPos = dragon.position;
+        draPos.y = 50;
+        draPos.z -= 10;
+        transform.position = Vector3.Lerp(transform.position, draPos, 0.3f);
+    }
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+}
---
+++ 3Dgame/Assets/Scripts/Dragon.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    [Header("移動速度"), Range(1, 1000)]
+    public float speed = 300;
+    [Header("虛擬搖桿")]
+    public Joystick joy;
+
+    /// <summary>
+    /// 移動方向
+    /// </summary>
+    public void Move()
+    {
+        print("移動中~");
+
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        transform.Translate(speed * Time.deltaTime * h, 0, speed * Time.deltaTime * v);
+
+        float joyV = joy.Vertical;
+        float joyH = joy.Horizontal;
+
+        transform.Translate(speed * Time.deltaTime * joyH, 0, speed * Time.deltaTime * joyV);
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, 17, 80);
+        transform.position = pos;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+}
